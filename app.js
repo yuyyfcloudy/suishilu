c94f62d5d57b40f037a1f622c4f31cc11fb6531b
yuyyfcloudy <2237194962@qq.com>
2026-07-24 13:35:41 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
+function getLunar(day){
+
+let lunar=[
+"初一",
+"初二",
+"初三",
+"初四",
+"初五",
+"初六",
+"初七",
+"初八",
+"初九",
+"初十",
+"十一",
+"十二",
+"十三",
+"十四",
+"十五",
+"十六",
+"十七",
+"十八",
+"十九",
+"二十",
+"廿一",
+"廿二",
+"廿三",
+"廿四",
+"廿五",
+"廿六",
+"廿七",
+"廿八",
+"廿九",
+"三十"
+];
+
+
+return lunar[(day-1)%30];
+
+}
+
 let notes =
 JSON.parse(localStorage.getItem("notes"))
 || [];
@@ -57,7 +97,16 @@ function createCalendar(){
 
         d.className="day";
 
-        d.innerHTML=i;
+        d.innerHTML=
+	`
+	<span class="lunar-day">
+	${getLunar(i)}
+	</span>
+
+	<span class="solar-day">
+	${i}
+	</span>
+	`;
 
 
         d.onclick=()=>{
